94ff7c4b657227ee07d49bb2f715b6e2dfa19122
imp4l3r <sogtrhs@gmail.com>
2021-01-31 13:33:32 +0100

    backup before formatting

--- i3/ultrahome.sh
+++ i3/ultrahome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 $(sleep 2s)
-#xrandr --output eDP --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 5360x0 --rotate left --output DisplayPort-0 --primary --mode 3440x1440 --pos 1920x0 --rotate normal
+xrandr --output eDP --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 5360x0 --rotate left --output DisplayPort-0 --primary --mode 3440x1440 --pos 1920x0 --rotate normal
 #
 #xset r rate 300 50
 setroot --on 0 Pictures/wallpapers/wallpaperguyincity.jpg --on 1 -z Pictures/wallpapers/wallpaper5.jpg --on 2 -z Pictures/wallpapers/wallpaperoctaviafull.jpeg
